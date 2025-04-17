d39527432557fdf061780e66195831782c4711d1
junhyukrim <junhyukrim@gmail.com>
2025-04-17 14:20:38 +0900

    google logo

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def google_button():
             width: auto;
             max-width: 400px;
             min-width: min-content;
+            margin: 0 auto;
         }
 
         .gsi-material-button .gsi-material-button-icon {
@@ -53,7 +54,7 @@ def google_button():
             -webkit-flex-wrap: nowrap;
             flex-wrap: nowrap;
             height: 100%;
-            justify-content: space-between;
+            justify-content: center;
             position: relative;
             width: 100%;
         }
@@ -119,7 +120,7 @@ def google_button():
         <div class="button-wrapper">
             <button class="gsi-material-button" onclick="parent.window.location.href=window.location.origin + '?oauth_trigger=True'">
                 <div class="gsi-material-button-content-wrapper">
-                    <img class="gsi-material-button-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+                    <img class="gsi-material-button-icon" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4="/>
                     <span class="gsi-material-button-contents">Google로 로그인</span>
                     <div class="gsi-material-button-state"></div>
                 </div>
