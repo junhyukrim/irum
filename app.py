90d02474ac747c91da329863774340c6da8a36e8
junhyukrim <junhyukrim@gmail.com>
2025-04-17 14:34:30 +0900

    login 화면

--- app.py
+++ app.py
@@ -35,6 +35,24 @@ def login_container():
             text-align: center;
             margin-bottom: 2rem;
         }
+
+        /* Streamlit 버튼 스타일 수정 */
+        .stButton button {
+            background-color: white !important;
+            color: #444 !important;
+            border: 1px solid #747775 !important;
+            border-radius: 4px !important;
+            padding: 0.5rem 1rem !important;
+            font-family: 'Roboto', sans-serif !important;
+            font-weight: 500 !important;
+            margin: 0 auto !important;
+            display: block !important;
+        }
+
+        .stButton button:hover {
+            background-color: #f8f8f8 !important;
+            box-shadow: 0 1px 2px 0 rgba(60, 64, 67, .30), 0 1px 3px 1px rgba(60, 64, 67, .15) !important;
+        }
         </style>
         <div class="login-container">
             <div class="logo-container">
@@ -47,142 +65,12 @@ def login_container():
         unsafe_allow_html=True
     )
 
-def google_button():
-    st.write(
-        """
-        <style>
-        .gsi-material-button {
-            -moz-user-select: none;
-            -webkit-user-select: none;
-            -ms-user-select: none;
-            -webkit-appearance: none;
-            background-color: WHITE;
-            background-image: none;
-            border: 1px solid #747775;
-            -webkit-border-radius: 4px;
-            border-radius: 4px;
-            -webkit-box-sizing: border-box;
-            box-sizing: border-box;
-            color: #1f1f1f;
-            cursor: pointer;
-            font-family: 'Roboto', arial, sans-serif;
-            font-size: 14px;
-            height: 40px;
-            letter-spacing: 0.25px;
-            outline: none;
-            overflow: hidden;
-            padding: 0 12px;
-            position: relative;
-            text-align: center;
-            -webkit-transition: background-color .218s, border-color .218s, box-shadow .218s;
-            transition: background-color .218s, border-color .218s, box-shadow .218s;
-            vertical-align: middle;
-            white-space: nowrap;
-            width: auto;
-            max-width: 400px;
-            min-width: min-content;
-            margin: 0 auto;
-        }
-
-        .gsi-material-button .gsi-material-button-icon {
-            height: 20px;
-            margin-right: 12px;
-            min-width: 20px;
-            width: 20px;
-        }
-
-        .gsi-material-button .gsi-material-button-content-wrapper {
-            -webkit-align-items: center;
-            align-items: center;
-            display: flex;
-            -webkit-flex-direction: row;
-            flex-direction: row;
-            -webkit-flex-wrap: nowrap;
-            flex-wrap: nowrap;
-            height: 100%;
-            justify-content: center;
-            position: relative;
-            width: 100%;
-        }
-
-        .gsi-material-button .gsi-material-button-contents {
-            -webkit-flex-grow: 1;
-            flex-grow: 1;
-            font-family: 'Roboto', arial, sans-serif;
-            font-weight: 500;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            vertical-align: top;
-        }
-
-        .gsi-material-button .gsi-material-button-state {
-            -webkit-transition: opacity .218s;
-            transition: opacity .218s;
-            bottom: 0;
-            left: 0;
-            opacity: 0;
-            position: absolute;
-            right: 0;
-            top: 0;
-        }
-
-        .gsi-material-button:disabled {
-            cursor: default;
-            background-color: #ffffff61;
-            border-color: #1f1f1f1f;
-        }
-
-        .gsi-material-button:disabled .gsi-material-button-contents {
-            opacity: 38%;
-        }
-
-        .gsi-material-button:disabled .gsi-material-button-icon {
-            opacity: 38%;
-        }
-
-        .gsi-material-button:not(:disabled):active .gsi-material-button-state,
-        .gsi-material-button:not(:disabled):focus .gsi-material-button-state {
-            background-color: #303030;
-            opacity: 12%;
-        }
-
-        .gsi-material-button:not(:disabled):hover {
-            -webkit-box-shadow: 0 1px 2px 0 rgba(60, 64, 67, .30), 0 1px 3px 1px rgba(60, 64, 67, .15);
-            box-shadow: 0 1px 2px 0 rgba(60, 64, 67, .30), 0 1px 3px 1px rgba(60, 64, 67, .15);
-        }
-
-        .gsi-material-button:not(:disabled):hover .gsi-material-button-state {
-            background-color: #303030;
-            opacity: 8%;
-        }
-
-        .button-wrapper {
-            display: flex;
-            justify-content: center;
-            width: 100%;
-            margin-top: 10px;
-        }
-        </style>
-        <div class="button-wrapper">
-            <button class="gsi-material-button" onclick="parent.window.location.href=window.location.origin + '?oauth_trigger=True'">
-                <div class="gsi-material-button-content-wrapper">
-                    <img class="gsi-material-button-icon" src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4="/>
-                    <span class="gsi-material-button-contents">Google로 로그인</span>
-                    <div class="gsi-material-button-state"></div>
-                </div>
-            </button>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
-
 def login_screen():
     login_container()
-    google_button()
+    st.button("Google로 로그인", on_click=st.login)
 
-if __name__ == "__main__":
-    if not st.experimental_user.is_logged_in:
-        login_screen()
-    else:
-        st.header(f"환영합니다, {st.experimental_user.name}님!")
-        st.button("로그아웃", on_click=st.logout) 
+if not st.experimental_user.is_logged_in:
+    login_screen()
+else:
+    st.header(f"환영합니다, {st.experimental_user.name}님!")
+    st.button("로그아웃", on_click=st.logout) 
