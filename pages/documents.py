d9b187a3309e6f7f14f58eb6fe722bd053569381
junhyukrim <junhyukrim@gmail.com>
2025-04-18 04:13:10 +0900

    file split

---
+++ pages/documents.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show_documents_page():
+    st.markdown('<h3 class="main-header">서류관리</h3>', unsafe_allow_html=True)
+    st.write("여기에 서류관리 내용이 들어갑니다.") 
---
+++ pages/jobs.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show_jobs_page():
+    st.markdown('<h3 class="main-header">공고관리</h3>', unsafe_allow_html=True)
+    st.write("여기에 공고관리 내용이 들어갑니다.") 
