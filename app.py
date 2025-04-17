62b5a4de0d8f80c7c248e27284a94dc86dd7dbaf
junhyukrim <junhyukrim@gmail.com>
2025-04-17 21:18:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ def main_screen():
             st.session_state.current_page = '서류관리'
 
         # 빈 공간 추가 (크기 조절)
-        st.markdown("<div style='flex-grow: 1; min-height: calc(100vh - 300px);'></div>", unsafe_allow_html=True)
+        st.markdown("<div style='flex-grow: 1; min-height: calc(100vh - 800px);'></div>", unsafe_allow_html=True)
         
         # 로그아웃 버튼
         st.button("로그아웃", key='logout', on_click=st.logout)
