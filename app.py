68db7b6462dcab44560a1774875dc7a334feb5de
junhyukrim <junhyukrim@gmail.com>
2025-04-18 01:18:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -241,11 +241,8 @@ def main_screen():
                 .stDateInput > div > div > input,
                 div[data-baseweb="input"] > input,
                 div[data-baseweb="input"],
-                div[class*="st-emotion-cache"] div[data-baseweb="input"],
-                div[class*="st-emotion-cache"] div[data-baseweb="input"] > input,
-                #tabs-bui222-tabpanel-0 > div > div > div > div:nth-child(4) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div > div > div > div > div > div > div,
-                #tabs-bui222-tabpanel-0 > div > div > div > div:nth-child(11) > div:nth-child(1) > div > div > div > div > div > div > div > div,
-                #tabs-bui222-tabpanel-0 > div > div > div > div:nth-child(11) > div:nth-child(2) > div > div > div > div > div > div > div > div {
+                div[data-baseweb="base-input"],
+                #tabs-bui222-tabpanel-0 > div > div > div > div:nth-child(4) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div > div > div > div > div > div > div {
                     background-color: #F8F9FA !important;
                 }
 
@@ -255,21 +252,15 @@ def main_screen():
                 .stDateInput > div > div > input:hover,
                 div[data-baseweb="input"] > input:hover,
                 div[data-baseweb="input"]:hover,
-                div[class*="st-emotion-cache"] div[data-baseweb="input"]:hover,
-                div[class*="st-emotion-cache"] div[data-baseweb="input"] > input:hover,
+                div[data-baseweb="base-input"]:hover,
                 #tabs-bui222-tabpanel-0 > div > div > div > div:nth-child(4) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div > div > div > div > div > div > div:hover,
-                #tabs-bui222-tabpanel-0 > div > div > div > div:nth-child(11) > div:nth-child(1) > div > div > div > div > div > div > div > div:hover,
-                #tabs-bui222-tabpanel-0 > div > div > div > div:nth-child(11) > div:nth-child(2) > div > div > div > div > div > div > div > div:hover,
                 .stTextInput > div > div > input:focus,
                 .stSelectbox > div > div > div:focus,
                 .stDateInput > div > div > input:focus,
                 div[data-baseweb="input"] > input:focus,
                 div[data-baseweb="input"]:focus-within,
-                div[class*="st-emotion-cache"] div[data-baseweb="input"]:focus-within,
-                div[class*="st-emotion-cache"] div[data-baseweb="input"] > input:focus,
-                #tabs-bui222-tabpanel-0 > div > div > div > div:nth-child(4) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div > div > div > div > div > div > div:focus-within,
-                #tabs-bui222-tabpanel-0 > div > div > div > div:nth-child(11) > div:nth-child(1) > div > div > div > div > div > div > div > div:focus-within,
-                #tabs-bui222-tabpanel-0 > div > div > div > div:nth-child(11) > div:nth-child(2) > div > div > div > div > div > div > div > div:focus-within {
+                div[data-baseweb="base-input"]:focus-within,
+                #tabs-bui222-tabpanel-0 > div > div > div > div:nth-child(4) > div:nth-child(2) > div > div > div > div > div:nth-child(2) > div > div > div > div > div > div > div > div:focus-within {
                     background-color: #FFFFFF !important;
                 }
                 
