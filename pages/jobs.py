6be2a4ecf41e1606b454b744ce52811489ee300f
junhyukrim <junhyukrim@gmail.com>
2025-04-18 17:56:10 +0900

    Update jobs.py

--- pages/jobs.py
+++ pages/jobs.py
@@ -49,8 +49,8 @@ def show_jobs_page():
             background-color: #FFFFFF !important;
         }
 
-        /* 일반 버튼 스타일링 */
-        div.stButton > button:not([data-testid="stSidebarNavButton"]) {
+        /* 버튼 스타일링 */
+        div.stButton > button:not([data-testid="baseButton-secondary"]) {
             width: 100% !important;
             height: 42px !important;
             margin: 0 !important;
@@ -66,12 +66,12 @@ def show_jobs_page():
             border: 1px solid #4285F4 !important;
         }
 
-        div.stButton > button:not([data-testid="stSidebarNavButton"]):hover {
+        div.stButton > button:not([data-testid="baseButton-secondary"]):hover {
             background-color: #1967D2 !important;
             border-color: #1967D2 !important;
         }
 
-        div.stButton > button:not([data-testid="stSidebarNavButton"]):active {
+        div.stButton > button:not([data-testid="baseButton-secondary"]):active {
             background-color: #1557B0 !important;
             border-color: #1557B0 !important;
         }
